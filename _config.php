c1a7cf05d464d8a23a0202c45283c07f6f48be7f
rasstislav <brandobur@webmax.sk>
2015-11-17 02:16:49 +0100

    Update _config.php

--- _config.php
+++ _config.php
@@ -2,3 +2,6 @@
 
 // Add new authenticator
 Authenticator::register_authenticator('UsernameOrEmailAuthenticator');
+
+Authenticator::set_default_authenticator("UsernameOrEmailAuthenticator"); // makes username authentication default
+Authenticator::unregister("MemberAuthenticator"); // removes default email + password authentication
